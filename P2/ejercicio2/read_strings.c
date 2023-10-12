c7c8024c4acc7ecc34d379c8488d8012cfb3deb9
A. Rubio <ampxrubio@gmail.com>
2023-10-12 13:00:20 +0200

    ejercicio 2

--- P2/ejercicio2/read_strings.c
+++ P2/ejercicio2/read_strings.c
@@ -16,12 +16,50 @@
  */
 char *loadstr(FILE *file)
 {
-	/* To be completed */
-	return NULL;	
+	    char* buffer = NULL;
+    size_t buffer_size = 0;
+
+    while (1) {
+        char c;
+        if (fread(&c, 1, 1, file) != 1)
+            break;
+
+        if (c == '\0') {
+            // Imprime la cadena y reinicia el búfer
+            printf("%s\n", buffer);
+            return buffer;
+        } else {
+            // Agrega el carácter al búfer
+            buffer = (char*)realloc(buffer, buffer_size + 1);
+            buffer[buffer_size] = c;
+            buffer_size++;
+        }
+    }
+
+    fclose(file);
+
+	return buffer;	
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <nombre_archivo>\n", argv[0]);
+        exit(1);
+    }
+
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error al abrir el archivo");
+        exit(2);
+    }
+
+	char* buffer= loadstr(file);
+    if (buffer != NULL) {
+        free(buffer);
+    }
+
+    return 0;
 }
 
-int main(int argc, char *argv[])
-{
-	/* To be completed */
-	return 0;
-}
