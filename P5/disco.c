823b233b71d2d6b1cfef89225b93b312684e2b1a
A. Rubio <ampxrubio@gmail.com>
2023-11-30 12:42:44 +0100

    planteado pero no probado, mi maquina virtual esta rota

--- P5/disco.c
+++ P5/disco.c
@@ -28,11 +28,39 @@ pthread_cond_t cond;
 
 void enter_normal_client(int id)
 {
+	//critical reg
+	int turn;
+	pthread_mutex_lock(&mutex);
+	turn = currentTicket++;
+	while(numClients>=CAPACITY || vipWaiting==0){
+		pthread_cond_wait(&cond, &mutex);
+	}
+	clientsInside++;
+	currentTurn++;
+	pthread_cond_broadcast(&cond);
+	pthread_mutex_unlock(&mutex);
+	
 
 }
 
 void enter_vip_client(int id)
 {
+	//critical reg
+	int turn;
+	pthread_mutex_lock(&mutex);
+	turn = currentTicket++;
+	currentVipTicket++;
+	
+	while(numClients>=CAPACITY){ //they are vip so they dont need to wait more
+		pthread_cond_wait(&cond, &mutex);
+	}
+	clientsInside++;
+	clientsVipInside++;
+	currentTurn++;
+	vipWaiting--;
+	pthread_cond_broadcast(&cond);
+	pthread_mutex_unlock(&mutex);
+
 
 }
 
@@ -45,6 +73,7 @@ void dance(int id, int isvip)
 void disco_exit(int id, int isvip)
 {
 
+
 }
 
 void *client(void *arg)
@@ -53,22 +82,19 @@ void *client(void *arg)
 
 	if(c!=nullptr){
 		printf("Hilo: %d y soy %d", c->id, c->isVip);
-
 		if(c->isVip){
 			enter_vip_client(c->id);
-			vipWaiting--;
 		}
 		else{
 			enter_normal_client(c->id);
 		}
-		currentTurn++;
-		clientsInside++;
 
 		dance(c->id, c->isVip);
-		disco_exit(c->id, c->isVip);
+		
+		free(c);
 	}
 	else{
-		perror("Bad Format");
+		perror("Bad Format of client_arg_t");
 	}
 }
 
@@ -78,27 +104,37 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Uso: %s <nombre_fichero>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
+	//open file
     FILE *file = fopen(argv[1], "r");
-    if (!file) {
+    //if file is null then exit bad
+	if (!file) {
         perror("Error al abrir el archivo");
         exit(EXIT_FAILURE);
     }
+	pthread_mutex_init(&mutex,NULL);
 
-    int M;
-    fscanf(file, "%d", &M);
-
-    pthread_t threads[MAX_CLIENTS]; // all the client threads
-    int ids[MAX_CLIENTS]; //all the clients could be normal clients
-    int vip_flags[MAX_CLIENTS]; //or vip ones
+    pthread_t* threads= malloc(MAX_CLIENTS* sizeof(pthread_t)); // all the client threads
+	if(threads==NULL){
+		perror("malloc bad");
+		exit(EXIT_FAILURE);
+	}
+    int ids[MAX_CLIENTS]; //all the id of the clients (0,1,2,3)
+    int vip_flags[MAX_CLIENTS]; //mark all the vip ones(1,0,0,1) 1=vip
 	
-	 for (int i = 0; i < M; i++) {
-		client_arg_t c= client_arg_t();
-		c->id=i;
-        fscanf(file, "%d", &vip_flags[i]);
-		c->isVip=&vip_flags[i];
+	//create all the threads
+	 for (int i = 0; i < MAX_CLIENTS; i++) {
+		client_arg_t* c= malloc(sizeof(client_arg_t));
+        c->id=i;
+		
+		if(fscanf(file, "%d", &c->isVip)=-1){
+			perror("format of %d is incorrect", i);
+			exit(EXIT_FAILURE);
+		}
+
+		vip_flags[i]= c->isVip;
+		ids[i]=c->id;
         pthread_create(&threads[i], NULL, client, c);
-        usleep(100); // Pequeña pausa entre la creación de hilos
+        usleep(100); //Little pause between threads
     }
 	
 	
