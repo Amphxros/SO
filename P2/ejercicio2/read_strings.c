d19b07550398307240e3fc2185904b624fd0ed8e
A. Rubio <ampxrubio@gmail.com>
2023-12-28 19:05:58 +0100

    Ejercicio 2 Practica 2

--- P2/ejercicio2/read_strings.c
+++ P2/ejercicio2/read_strings.c
@@ -1,27 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <err.h>
+char* loadstr(FILE* input) {
+    // Determine the length of the string
+    long pos = ftell(input);
+    long length = 0;
 
-/** Loads a string from a file.
- *
- * file: pointer to the FILE descriptor
- *
- * The loadstr() function must allocate memory from the heap to store
- * the contents of the string read from the FILE.
- * Once the string has been properly built in memory, the function returns
- * the starting address of the string (pointer returned by malloc())
- *
- * Returns: !=NULL if success, NULL if error
- */
-char *loadstr(FILE *file)
-{
-	/* To be completed */
-	return NULL;	
+    while (fgetc(input) != '\0') {
+        length++;
+    }
+
+    // Return to the starting position
+    fseek(input, pos, SEEK_SET);
+
+    // Allocate memory for the string
+    char* str = (char*)malloc(length + 1);
+    if (!str) {
+        perror("Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the string
+    fread(str, 1, length + 1, input);
+
+    return str;
 }
 
-int main(int argc, char *argv[])
-{
-	/* To be completed */
+void read_strings( FILE* file){
+	while(!feof(file)){
+		char * line= loadstr(file);
+		if(feof(file)){
+			free(line);
+			break;
+		}
+
+		printf("%s\n", line);
+		free(line);
+	}
+}
+
+int main(int argc, char* argv[]){
+	if(argc !=2){
+		  fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        exit(1);
+	}
+
+	FILE* file= fopen(argv[1], "rb");
+
+	if(!file){
+		perror("File not existent");
+		exit(1);
+	}
+	
+	read_strings(file);
 	return 0;
+
 }
--- P2/ejercicio2/write_strings.c
+++ P2/ejercicio2/write_strings.c
@@ -3,8 +3,26 @@
 #include <string.h>
 #include <err.h>
 
+void write_strings(FILE* file, int argc, char* argv[]){
+	for(int i=2; i<argc;i++){
+		fwrite(argv[i], 1, strlen(argv[i]) + 1, file);
+	}
+}
+
 int main(int argc, char* argv[])
 {
-	/* To be completed */
+	if (argc < 3) {
+        fprintf(stderr, "Usage: %s <output_file> <string1> <string2> ...\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+	 FILE* file = fopen(argv[1], "wb");
+    if (!file) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    write_strings(file, argc, argv);
+	fclose(file);
+	
 	return 0;
 }
